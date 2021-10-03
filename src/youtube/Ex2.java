4ad4719882789d85f73351be8db73e92ec4a5401
Anton-Protasov <89267016232@mail.ru>
2021-10-03 17:18:20 +0300

    05. Find in the range

--- src/youtube/Ex2.java
+++ src/youtube/Ex2.java
@@ -23,5 +23,13 @@ public class Ex2 {
         while (m2.find()) {
             System.out.print(m2.start() + " " + m2.group() + " ");
         }
+        System.out.println();
+
+        // option 3 "[0-9]+" - searching not only one digital but all digital (number)
+        Pattern p3 = Pattern.compile("[0-9]+");
+        Matcher m3 = p3.matcher("a1b12c240");
+        while (m3.find()) {
+            System.out.println("position " + m3.start() + "of symbol: " + m3.group() + " ");
+        }
     }
 }
